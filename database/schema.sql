9ba59028a6b1cfdc874408f401b2ffd440e5c3b0
Ramon Bautista <rb11001001@gmail.com>
2025-01-22 23:01:20 -0500

    Updated schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,26 +1,28 @@
--- DROP TABLE IF EXISTS cities
--- DROP TABLE IF EXISTS hourly_weather
--- DROP TABLE IF EXISTS forecast
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS hourly_weather;
+DROP TABLE IF EXISTS forecast;
 
 CREATE TABLE IF NOT EXISTS cities (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     state TEXT NOT NULL,
-    coordinates TEXT NOT NULL,
-    forecast_url TEXT NOT NULL,
-    hourly_url TEXT NOT NULL,
     country TEXT,
-    timezone TEXT
+    longitude REAL NOT NULL,
+    latitude REAL NOT NULL,
+    timezone TEXT,
+    hourly_url TEXT NOT NULL,
+    forecast_url TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS hourly_weather (
     id INTEGER PRIMARY KEY,
     city_id INTEGER NOT NULL,
     start_time TEXT,
-    end_time TEXT,
     termperature INTEGER,
     termperature_unit TEXT,
     precipitation_probability INTEGER,
+    rain REAL,
+    snow REAL,
     dewpoint REAL,
     humidity INTEGER,
     wind_speed TEXT,
@@ -38,6 +40,8 @@ CREATE TABLE IF NOT EXISTS forecast (
     temperature INTEGER,
     termperature_unit TEXT,
     precipitation_probability INTEGER,
+    rain REAL,
+    snow REAL,
     wind_speed TEXT,
     wind_direction TEXT,
     forcast_short_desc TEXT,
