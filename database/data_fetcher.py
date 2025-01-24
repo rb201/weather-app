cfcb11d478d6b6d97639a9f9140652dc50524be5
Ramon Bautista <rb11001001@gmail.com>
2025-01-23 22:26:27 -0500

    <WIP> Refactor

--- database/data_fetcher.py
+++ database/data_fetcher.py
@@ -32,166 +32,173 @@ class DatabaseConnection:
     
 
 class WeatherFetcher:
-    def __init__(self, ):
-        pass
+    def __init__(
+        self,
+        db_path: str,
+        # coordinates: list = None,
+        latitude: float = None,
+        longitude: float = None,
+        city: str = None,
+        state: str = None
+    ):
+        if longitude is None and latitude is None and city is None and state is None:
+            print("Please include the coordinates of your location, or city and state")
+            return
+
+        self.db = DatabaseConnection(db_path)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.city = city
+        self.state = state
+
+
+    def add_city_to_db(
+        self,
+        name: str,
+        state: str,
+        country: str,
+        timezone: str,
+        latitude: float,
+        longitude: float,
+        hourly_url: str,
+        forecast_url: str
+    ):
+        db_conn = self.db.get_connection()
+
+        with db_conn as conn:
+            try:
+                cur = conn.cursor()
+
+                add_city_query = (
+                    """
+                    INSERT INTO cities
+                    (
+                        name, state, country, timezone,
+                        longitude, latitude, hourly_url, forecast_url
+                    )
+                    VALUES
+                    (?, ?, ?, ?, ?, ?, ?, ?)
+                    """
+                )
+
+                cur.execute(
+                    add_city_query,
+                    (
+                        name, state, country, timezone,
+                        longitude, latitude, hourly_url, forecast_url
+                    )
+                )
+
+                conn.commit()
+            except sqlite3.Error as sqle:
+                print(f"Error adding city to database.cities: {sqle}")
 
 
     def get_current_weather(self, ):
         pass
 
 
-    def get_forecast(self,):
+    def get_city_from_lon_lat(self, latitude: float, longitude: float):
+        geolocater = Nominatim(user_agent = 'my-weather-app')
+
+        location = geolocater.reverse(str(latitude) + ',' + str(longitude))
+
+        city = location.raw['address']['city']
+        state = location.raw['address']['state']
+        return city, state
+    
+    
+    def get_coor_from_city_state(self, city: str, state: str):
+        # geolocater = Nominatim(user_agent = 'my-weather-app')
         pass
 
 
-    def get_historical_weather(self, ):
-        pass
+    def if_city_in_db(self, city, state) -> bool:
+        db_conn = self.db.get_connection()
 
-def get_db():
-    # db = DatabaseConnection()
-    pass
+        info = [city, state]
 
-def if_city_in_db(city, state) -> bool:
-    DATABASE = "weather.db"
-    
-    db = DatabaseConnection(DATABASE)
-    
-    with db.get_connection() as conn:
-        try:
-            cur = conn.cursor()
-            
-            query = (
-                """
-                SELECT name, state
-                FROM cities 
-                WHERE name = ? and state = ?
-                """
-            )
-            
-            cur.execute(query, (city, state))
-
-            read_output = cur.fetchall()
-            if not read_output:
-                return False
-            return True
+        with db_conn as conn:
+            try:
+                cur = conn.cursor()
                 
-        except sqlite3.Error as sqle:
-            print(f"Error has occured: {sqle}")
-            
-
-def get_city_from_lon_lat(latitude: float, longitude: float):
-    geolocater = Nominatim(user_agent = 'my-weather-app')
-    
-    location = geolocater.reverse(str(latitude) + ',' + str(longitude))
-    
-    print(location)
-    print(location.raw['address'])
-    city = location.raw['address']['city']
-    state = location.raw['address']['state']
-    return city, state
-
-
-def add_city_to_db(
-    name: str,
-    state: str,
-    country: str,
-    timezone: str,
-    longitude: float,
-    latitude: float,
-    hourly_url: str,
-    forecast_url: str
-):
-    db = DatabaseConnection("weather.db")
-
-    with db.get_connection() as conn:
-        try:
-            cur = conn.cursor()
-            
-            add_city_query = (
-                """
-                INSERT INTO cities
-                (
-                    name, state, country, timezone,
-                    longitude, latitude, hourly_url, forecast_url
+                query = (
+                    """
+                    SELECT name, state
+                    FROM cities
+                    WHERE name = ? and state = ?
+                    """
                 )
-                VALUES 
-                (?, ?, ?, ?, ?, ?, ?, ?)
-                """
-            )
-            
-            cur.execute(
-                add_city_query,
-                (
-                    name, state, country, timezone,
-                    longitude, latitude, hourly_url, forecast_url
-                )
-            )
-            
-            conn.commit()
-        except sqlite3.Error as sqle:
-            print(f"Error adding city to database.cities: {sqle}")
-            
+
+                cur.execute(query, *info)
+
+                city_in_db = cur.fetchone()
+
+                return False if not city_in_db else True
+
+            except sqlite3.Error as sqle:
+                print(f"Error has occured: {sqle}")
+
+
+    def write_current_weather_to_db(self, city, state, data):
+        db_conn = self.db.get_connection()
         
-
-def write_current_weather_to_db(data):
-    DATABASE = "weather.db"
-    
-    temperature = data("temperature")
-    apparent_temperature = data["apparent_temperature"]
-    temperature_min = data["temperature_min"]
-    temperature_max = data["temperature_max"]
-    pressure = data["pressure"]
-    humidty = data["humidity"]
-    forecast_main_description = data["forecast_main_description"],
-    forecast_short_description = data["forecast_short_description"],
-    sunrise = data["sunrise"]
-    sunset = data["sunset"]
-    timestamp_calc = data["timestamp_calc"],
-    wind_speed = data["wind_speed"],
-    wind_gust = data["wind_gust"],
-    rain = data["rain"],
-    snow = data["snow"],
-    visibility = data["visibility"],
-    
-    db = DatabaseConnection(DATABASE)
-    
-    with db.get_connection() as conn:
-        try:
-            cur = conn.cursor()
-            
-            cur.execute("""
-                INSERT INTO hourly_weather (
-                    city_id,
-                    temperature,
-                    apparent_temperature, 
-                    temperature_min,
-                    temperature_max,
-                    pressure,
-                    humidty,
-                    forecast_main_description,
-                    forecast_short_description,
-                    sunrise,
-                    sunset,
-                    timestamp_calc,
-                    wind_speed,
-                    wind_gust,
-                    rain,
-                    snow,
-                    visibility)
-                VALUES (
-                    (SELECT id FROM cities WHERE name = "Jersey CIty" and state = "NJ"), 
-                    ?, ?, ?,
-                    ?, ?, ?, ?,
-                    ?, ?, ?, ?,
-                    ?, ?, ?, ?, ?
-                )
-            """, data)
+        temperature = data("temperature")
+        apparent_temperature = data["apparent_temperature"]
+        temperature_min = data["temperature_min"]
+        temperature_max = data["temperature_max"]
+        pressure = data["pressure"]
+        humidty = data["humidity"]
+        forecast_main_description = data["forecast_main_description"],
+        forecast_short_description = data["forecast_short_description"],
+        sunrise = data["sunrise"]
+        sunset = data["sunset"]
+        timestamp_calc = data["timestamp_calc"],
+        wind_speed = data["wind_speed"],
+        wind_gust = data["wind_gust"],
+        rain = data["rain"],
+        snow = data["snow"],
+        visibility = data["visibility"],
         
-        except sqlite3.Error as sqle:
-            print(sqle)
+        with db_conn as conn:
+            try:
+                cur = conn.cursor()
+
+                cur.execute(f"""
+                    INSERT INTO hourly_weather (
+                        city_id,
+                        temperature,
+                        apparent_temperature,
+                        temperature_min,
+                        temperature_max,
+                        pressure,
+                        humidty,
+                        forecast_main_description,
+                        forecast_short_description,
+                        sunrise,
+                        sunset,
+                        timestamp_calc,
+                        wind_speed,
+                        wind_gust,
+                        rain,
+                        snow,
+                        visibility)
+                    VALUES (
+                        (SELECT id FROM cities WHERE name = {city} and state = {state}),
+                        ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?, ?,
+                        ?, ?,
+                    )
+                """, data)
+
+            except sqlite3.Error as sqle:
+                print(sqle)
 
 
 db_file = "weather.db"
+
+weather_obj = WeatherFetcher(db_file)
 # jc_forecast_file = "forcast.json"
 # jc_forecast_hourly_file = "forcast_hourly.json"
 jc_current_file = "owm_current.json"
@@ -226,12 +233,15 @@ parsed_data = {
     "rain": data.get("rain", None),
     "snow": data.get("snow", None),
     "visibility": data.get("visibility", None),
+    "timezone": data.get("timezone", None)
 }
 
 # print(jcf_start_time)
 # write_to_db(parsed_data)
 latitude = 40.719074
 longitude = -74.050552
+timezone = parsed_data["timezone"]
+base_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid=API_KEY"
 country = "US"
 # city, state = get_city_from_lon_lat(lat, lon)
 city = "Jersey City"
@@ -246,7 +256,7 @@ if not city_in_db:
         country = country,
         longitude = longitude,
         latitude = latitude,
-        timezone = "5gmt",
-        hourly_url = "https://example.com/hourly",
+        timezone = timezone,
+        hourly_url = base_url,
         forecast_url = "https://example.com/forecast"
     )
