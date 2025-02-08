e02d51540da2ac44a6792067e64ae73a56d1ef9e
Ramon Bautista <rb11001001@gmail.com>
2025-02-08 15:28:20 -0500

    Moved button to navbar

--- frontend/weather-app/src/components/Nav/Nav.jsx
+++ frontend/weather-app/src/components/Nav/Nav.jsx
@@ -1,14 +1,17 @@
+import './Nav.css'
+
 export default function Navigation(props) {
     return (
         <>
             <nav className="top-navigation">
                 <search>
                     <form htmlFor="search" method="GET" className="form-search">
-                        <div>
-                            <input name="search" type="text" placeholder="City" /> 
-                        </div>
+                        <input name="search" type="text" placeholder="City" /> 
                     </form>
                 </search>
+                <button className="weather-container__current-title-bar-tenp-button">
+                    C to F
+                </button>
             </nav>
         </>
     )
