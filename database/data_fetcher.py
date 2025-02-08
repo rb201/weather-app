f30689d106f7b892a65aa1c0657defd9ca9c32b6
Ramon Bautista <rb11001001@gmail.com>
2025-02-08 15:24:56 -0500

    Add function to get data from db

--- database/data_fetcher.py
+++ database/data_fetcher.py
@@ -103,6 +103,7 @@ class WeatherFetcher:
 
         return city_id
 
+
     def get_current_weather(self):
         url = f"https://api.openweathermap.org/data/2.5/weather?lat={self.latitude}&lon={self.longitude}&units=metric&appid={API_KEY}"
 
@@ -163,6 +164,54 @@ class WeatherFetcher:
             data = parsed_current_weather_data
         )
 
+
+    def get_current_weather_from_db(self):
+        info = [self.city, self.state]
+
+        if not self.city and self.state:
+            return {"message": "error, city and/or state not defined"}
+
+        db_conn = self.db.get_connection()
+
+        with db_conn as conn:
+            try:
+                cur = conn.cursor()
+
+                query = """
+                    SELECT hw.*
+                    FROM hourly_weather hw
+                    JOIN cities c on hw.city_id
+                    WHERE name = ? and state = ?
+                    ORDER BY hw.id DESC
+                    LIMIT 1
+                """
+
+                query_data = cur.execute(query, info).fetchone()
+
+            except sqlite3.Error as sqle:
+                print(f"Error has occurred {sqle}")
+
+        cur_weather_obj = {
+            "temperature": query_data[2],
+            "apparent_temperature": query_data[3],
+            "temperature_min": query_data[4],
+            "temperature_max": query_data[5],
+            "pressure": query_data[6],
+            "humidity": query_data[7],
+            "forecast_main_description": query_data[8],
+            "forecast_short_description": query_data[9],
+            "sunrise": query_data[10],
+            "sunset": query_data[11],
+            "timestamp_calc": query_data[12],
+            "wind_speed": query_data[13],
+            "wind_direction": query_data[14],
+            "wind_gust": query_data[15],
+            "rain": query_data[16],
+            "snow": query_data[17],
+            "visibility": query_data[18]
+        }
+
+        return json.dumps(cur_weather_obj)
 
     def get_city_from_lon_lat(self):
         geolocater = Nominatim(user_agent = 'my-weather-app')
@@ -266,8 +315,8 @@ class WeatherFetcher:
                 print(sqle)
 
 
-db_file = "weather.db"
-lat, long = 40.719074, -74.050552
+# db_file = "weather.db"
+# lat, long = 40.719074, -74.050552
 
-weather_obj = WeatherFetcher(db_file, longitude = long, latitude = lat)
-weather_obj.get_current_weather()
+# weather_obj = WeatherFetcher(db_file, longitude = long, latitude = lat)
+# weather_obj.get_current_weather()
