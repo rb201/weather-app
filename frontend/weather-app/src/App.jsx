b9259470593942fcd0a16b96870bad16f73247dc
Ramon Bautista <rb11001001@gmail.com>
2025-02-09 22:12:29 -0500

    Refactored weather section, moved to component

--- frontend/weather-app/src/App.jsx
+++ frontend/weather-app/src/App.jsx
@@ -1,103 +1,23 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
 
 import Navigation from "./components/Nav/Nav";
 import WeatherSection from "./components/WeatherSection/WeatherSection";
 
-import sunIcon from "./assets/weather-icons/sun.png";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         <Navigation />
-
       </div>
       <div className="weather-container__current">
         <div className="weather-alert">{/* <h3>alert</h3> */}</div>
         <div className="weather-container__current-main">
-          <div className="weather-container__current-primary-info">
-            <p className="weather-container__current-main-location">
-              Jersey City, NJ, USA
-            </p>
-            <img
-              src={sunIcon}
-              className="weather-container__current-logo"
-              alt="sun"
-            />
-            <p className="weather-container__current-main-desc">Sunny</p>
-            <h2 className="weather-container__current-main-temp">19°C</h2>
-            <p className="weather-container__current-main-feels-like">
-              Feels like 18°C
-            </p>
-          </div>
           <WeatherSection />
-
-          {/* <div className="weather-container__current-secondary-infos">
-            <div className="weather-container__current-secondary-info">
-              <p className="weather-container__current-secondary-info-item">
-                Humidity
-              </p>
-              <p className="weather-container__current-secondary-info-item">
-                73%
-              </p>
-            </div>
-            <div className="weather-container__current-secondary-info">
-              <p className="weather-container__current-secondary-info-item">
-                Wind
-              </p>
-              <p className="weather-container__current-secondary-info-item">
-                3
-              </p>
-            </div>
-            <div className="weather-container__current-secondary-info">
-              <p className="weather-container__current-secondary-info-item">
-                Pressure
-              </p>
-              <p className="weather-container__current-secondary-info-item">1013 hPa</p>
-            </div>
-            <div className="weather-container__current-secondary-info">
-              <p className="weather-container__current-secondary-info-item">
-                Visibility
-              </p>
-              <p className="weather-container__current-secondary-info-item">
-                10km
-              </p>
-            </div>
-            <div className="weather-container__current-secondary-info">
-              <p className="weather-container__current-secondary-info-item">
-                Sunset
-              </p>
-              <p className="weather-container__current-secondary-info-item">
-                6:45pm
-              </p>
-            </div>
-            <div className="weather-container__current-secondary-info">
-              <p className="weather-container__current-secondary-info-item">Rain</p>
-              <p className="weather-container__current-secondary-info-item">1 inch</p>
-            </div>
-          </div> */}
         </div>
         <div className="weather-container__hourly">
           <p>hourly</p>
         </div>
       </div>
-      {/* <h1>Vite + React</h1> */}
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        {/* <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-      </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
--- frontend/weather-app/src/components/WeatherSection/WeatherSection.jsx
+++ frontend/weather-app/src/components/WeatherSection/WeatherSection.jsx
@@ -1,52 +1,86 @@
 import { useEffect, useState } from "react";
 
-export default function WeatherSection() {
-  const [weatherData, setWeaatherData] = useState(null);
+import sunIcon from "../../assets/weather-icons/sun.png";
 
-  const primaryData = {}
-  const secondaryData = {}
+
+export default function WeatherSection() {
+  const [ weatherData, setWeaatherData ] = useState(null);
+  const [ isLoadingData, setIsLoadingData ] = useState(true);
 
   useEffect(() => {
     const fetchWeatherData = async () => {
-      const res = await fetch("http://localhost:8000/current/");
-      const currentWeatherData = await res.json();
+        try {
+            const res = await fetch("http://localhost:8000/current/");
+            const currentWeatherData = await res.json();
 
-      if (!res.ok) {
-        throw new Error("Network response was not ok");
-      }
+            if (!res.ok) {
+              throw new Error("Network response was not ok");
+            }
 
-      setWeaatherData(JSON.parse(currentWeatherData));
+            setWeaatherData(JSON.parse(currentWeatherData));
+            setIsLoadingData(false)
+        } catch (error) {
+            console.log(error)
+        }
     };
 
     fetchWeatherData();
   }, []);
 
-  function parseSecWeatherData() {
-    secondaryData["Humidity"] = weatherData['humidity'];
-    secondaryData["Wind"] = weatherData["wind_speed"],
-    secondaryData["Pressure"] = weatherData["pressure"],
-    secondaryData["Visibility"] = weatherData["visibility"],
-    secondaryData["Sunset"] = weatherData["sunset"]
-    secondaryData["Rain"] = weatherData["rain"]
+  function parsePrimaryWeatherData() {
+    if (isLoadingData) return <div>Loading...</div>
+
+    const temperature = weatherData["temperature"];
+    const apparent_temperature = weatherData["apparent_temperature"];
+    const description = weatherData["forecast_main_description"];
+
+    return (
+      <div className="weather-container__current-primary-info">
+        <p className="weather-container__current-main-location">Jersey City, NJ, USA</p>
+        <img
+          src={sunIcon}
+          className="weather-container__current-logo"
+          alt="sun"
+        />
+        <p className="weather-container__current-main-desc">{description}</p>
+        <h2 className="weather-container__current-main-temp">{temperature}°C</h2>
+        <p className="weather-container__current-main-feels-like">
+          Feels like {apparent_temperature}°C
+        </p>
+      </div>
+    );
   }
 
+  function parseSecWeatherData() {
+    if (isLoadingData) return <div>Loading...</div>
 
-  if (weatherData) {
-    // console.log(weatherData);
-    parseSecWeatherData()
-    console.log(secondaryData)
+    const secondaryData = {};
+
+    secondaryData["Humidity"] = weatherData["humidity"];
+    secondaryData["Wind"] = weatherData["wind_speed"];
+    secondaryData["Pressure"] = weatherData["pressure"];
+    secondaryData["Visibility"] = weatherData["visibility"];
+    secondaryData["Sunset"] = weatherData["sunset"];
+    secondaryData["Rain"] = weatherData["rain"];
+
+    return (
+      <div className="weather-container__current-secondary-infos">
+        {Object.keys(secondaryData).map((key, idx) => {
+          return (
+            <div key={idx} className="weather-container__current-secondary-info">
+              <p key={key + "_key"}className="weather-container__current-secondary-info-item">{key}</p>
+              <p key={key + "_val"} className="weather-container__current-secondary-info-item">{secondaryData[key]}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 
   return (
     <>
-        <div className="weather-container__current-secondary-infos">
-            {Object.keys(secondaryData).map((key) => {
-                return (<div className="weather-container__current-secondary-info">
-                    <p key={key} className="weather-container__current-secondary-info-item">{key}</p>
-                    <p key={key} className="weather-container__current-secondary-info-item">{secondaryData[key]}</p>
-                </div>)
-            })}
-        </div>
+        {parsePrimaryWeatherData()}
+        {parseSecWeatherData()}
     </>
-  );
+  )
 }
