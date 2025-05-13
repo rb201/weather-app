2dedb7a5644c00c492c043ea7c16c2aad61d68fa
Ramon Bautista <rb11001001@gmail.com>
2025-05-13 19:10:21 -0400

    Fixed import path

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.data_fetcher import WeatherFetcher
+from .data_fetcher import WeatherFetcher
 
 origins = ["http://localhost:5173"]
 
