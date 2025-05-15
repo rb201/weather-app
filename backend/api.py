241d2655c6423f8429ae0b590fa81f2cf16430b1
Ramon Bautista <rb11001001@gmail.com>
2025-05-14 20:27:19 -0400

    formatting

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_client import generate_latest, CollectorRegistry, Counter
 
 from .data_fetcher import WeatherFetcher
 
@@ -8,16 +9,40 @@ origins = ["http://localhost:5173"]
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["GET"])
 
+metrics_registry = CollectorRegistry()
 
-@app.get("/health")
-async def health_status():
-    return {"message": "ok"}
+http_requests_total = Counter(
+    name="http_requests_total",
+    documentation="Total number of HTTP requests received",
+    registry=metrics_registry,
+    labelnames=["return_status", "route", "method"],
+)
+
+
+async def http_metrics(request: Request):
+    http_requests_total.labels(
+        return_status=str(200), route=request.url.components.path, method=request.method
+    ).inc()
+
+    yield
+
+    # maybe here latency metric
 
 
 @app.get("/current/")
-async def get_current_weather(city: str, state: str):
+async def get_current_weather(city: str, state: str, dep: str = Depends(http_metrics)):
     wf = WeatherFetcher("weather.db", city=city, state=state)
     current_weather_data = wf.get_current_weather()
     print(current_weather_data)
 
     return current_weather_data
+
+
+@app.get("/health")
+async def health_status(dep: str = Depends(http_metrics)):
+    return {"message": "ok"}
+
+
+@app.get("/metrics")
+def metrics():
+    return generate_latest(metrics_registry)
