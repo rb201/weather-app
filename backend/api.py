41b753c2ce781bb11990a942f634408c87eb8407
Ramon Bautista <rb11001001@gmail.com>
2025-05-15 18:35:46 -0400

    formatting

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from prometheus_client import generate_latest, CollectorRegistry, Counter
+from prometheus_client import generate_latest, CollectorRegistry, Counter, Gauge
 
 from .data_fetcher import WeatherFetcher
 
@@ -11,6 +11,12 @@ app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["GET"])
 
 metrics_registry = CollectorRegistry()
 
+active_http_requests = Gauge(
+    name="active_http_requests",
+    documentation="Current active HTTP requests",
+    registry=metrics_registry,
+)
+
 http_requests_total = Counter(
     name="http_requests_total",
     documentation="Total number of HTTP requests received",
@@ -19,18 +25,22 @@ http_requests_total = Counter(
 )
 
 
-async def http_metrics(request: Request):
+async def http_requests_metrics(request: Request):
+    active_http_requests.inc()
+
     http_requests_total.labels(
         return_status=str(200), route=request.url.components.path, method=request.method
     ).inc()
 
     yield
 
-    # maybe here latency metric
+    active_http_requests.dec()
 
 
 @app.get("/current/")
-async def get_current_weather(city: str, state: str, dep: str = Depends(http_metrics)):
+async def get_current_weather(
+    city: str, state: str, dep: str = Depends(http_requests_metrics)
+):
     wf = WeatherFetcher("weather.db", city=city, state=state)
     current_weather_data = wf.get_current_weather()
     print(current_weather_data)
@@ -39,10 +49,10 @@ async def get_current_weather(city: str, state: str, dep: str = Depends(http_met
 
 
 @app.get("/health")
-async def health_status(dep: str = Depends(http_metrics)):
-    return {"message": "ok"}
+async def health_status(dep: str = Depends(http_requests_metrics)):
+    return {"service": "ok"}
 
 
 @app.get("/metrics")
 def metrics():
-    return generate_latest(metrics_registry)
+    return generate_latest(metrics_registry).decode("utf8").split("\n")
