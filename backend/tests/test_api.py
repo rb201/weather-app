6c8a3980871ca01f8edd610df11b3849c4b65a55
Ramon Bautista <rb11001001@gmail.com>
2025-05-16 11:06:07 -0400

    Updated test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -4,14 +4,21 @@ from backend.api import app
 
 client = TestClient(app)
 
+
 def test_health_check_endpoint():
     res = client.get("/health")
-    
+
     assert res.status_code == 200
-    assert res.json() == {"message": "ok"}
+    assert res.json() == {"service": "ok"}
 
 
 def test_health_check_method():
     res = client.post("/health")
-    
+
     assert res.status_code == 405
+
+
+def test_current_endpoint_with_query_param():
+    res = client.get("/current/?city=jersey city&state=NJ")
+
+    assert res.status_code == 200
