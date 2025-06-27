d82bc64261575f5ef51f3e6fe4a09cf42a07d1be
Ramon Bautista <rb11001001@gmail.com>
2025-06-27 11:25:03 -0400

    Terraform fmt; ignore terraform files

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 # resource "aws_instance" "eks_worder_node" {
