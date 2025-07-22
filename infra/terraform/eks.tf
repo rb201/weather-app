7e162ad2bb0771c75cb565e39f16a101b8057273
Ramon Bautista <rb11001001@gmail.com>
2025-07-22 17:20:13 -0400

    EKS network config

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -1,9 +1,10 @@
+
 resource "aws_eks_node_group" "eks_worker_nodes" {
   cluster_name    = aws_eks_cluster.eks.name
-  instance_types = "t2.micro"
+  instance_types = ["t2.micro"]
   node_group_name = "weather-eks-worker-nodes"
   node_role_arn   = aws_iam_role.eks_worker_node_group_role.arn
-  subnet_ids      = ""
+  subnet_ids      = aws_subnet.eks_subnets[*].id
 
   scaling_config {
     desired_size = 3
@@ -19,6 +20,8 @@ resource "aws_eks_node_group" "eks_worker_nodes" {
     aws_iam_role_policy_attachment.eks_node_group_worker_node_policy,
     aws_iam_role_policy_attachment.eks_node_group_cni_policy,
     aws_iam_role_policy_attachment.eks_node_group_ecr_policy,
+    aws_iam_role_policy_attachment.eks_loadBalancing_policy,
+    aws_iam_role_policy_attachment.eks_networking_policy,
   ]
 }
 
@@ -29,12 +32,30 @@ resource "aws_eks_cluster" "eks" {
     authentication_mode = "API"
   }
 
+  bootstrap_self_managed_addons = false
+
+  compute_config {
+    enabled       = true
+    node_pools    = ["general-purpose"]
+    node_role_arn = aws_iam_role.eks_worker_node_group_role.arn
+  }
+
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = true
+    }
+  }
+
+  storage_config {
+    block_storage {
+      enabled = true
+    }
+  }
+
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [
-        
-    ]
+    subnet_ids = [ for subnet in aws_subnet.eks_subnets : subnet.id ]
   }
 
   depends_on = [
--- infra/terraform/networking.tf
+++ infra/terraform/networking.tf
@@ -1,7 +1,24 @@
-resource "aws_vpc" "main" {
+data "aws_availability_zones" "az_avail" {
+  state = "available"
+}
+
+resource "aws_vpc" "eks_vpc" {
   cidr_block       = "10.0.0.0/16"
 
   tags = {
-    Name = "jrzyproj"
+    Name = var.project_name_prefix
+  }
+}
+
+
+resource "aws_subnet" "eks_subnets" {
+  count = length(data.aws_availability_zones.az_avail.names)
+
+  availability_zone = data.aws_availability_zones.az_avail.names[count.index]
+  cidr_block = "10.0.${count.index}.0/24"
+  vpc_id     = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = var.project_name_prefix
   }
 }
